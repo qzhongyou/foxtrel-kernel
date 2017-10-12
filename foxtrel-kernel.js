6bb26c29d046c32fbfc715e1363f1c3fe6d9cf9f
阙中友 <quezhongyou@zhubajie.com>
2017-10-12 20:31:43 +0800

    新建lib

---
+++ foxtrel-kernel.js
@@ -0,0 +1,34 @@
+/**
+ * @authors       qzhongyou
+ * @date          2017-10-10 17:34:31
+ * @description   foxtrel 功能集成
+ */
+
+'use strict';
+var foxtrel = module.exports = {};
+
+//register global variable
+Object.defineProperty(global, 'foxtrel', {
+    enumerable : true,
+    writable : false,
+    value : foxtrel
+});
+
+//日志
+foxtrel.log = require("./lib/log.js");
+
+//require插件请求
+foxtrel.require = require("./lib/require.js");
+//util工具
+foxtrel.util =require("./lib/util.js");
+
+//配置
+foxtrel.config = require("./lib/config.js");
+
+//项目信息
+foxtrel.project =require("./lib/project.js");
+
+//uri资源
+foxtrel.uri =require("./lib/uri.js");
+
+
